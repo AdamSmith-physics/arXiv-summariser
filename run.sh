32b7452694726c600b71e21c348092dfff8e99ac
Adam Smith <adam.smith.home@ntlworld.com>
2025-12-01 12:07:20 +0000

    update shell script

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source activate arxiv-summariser
-nohup python -u run_summariser.py > logs/summariser_log.out 2>&1 &
+PYTHON = "/opt/anaconda3/envs/arxiv-summariser/bin/python"
+
+# source activate arxiv-summariser
+nohup /opt/anaconda3/envs/arxiv-summariser/bin/python -u run_summariser.py > logs/summariser_log.out 2>&1 &
 
 echo "Summariser is running in the background. Check summariser_log.txt for output."
